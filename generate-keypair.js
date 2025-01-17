47e9137318fd0c35f72ff08f310edba3700129e1
joexy <josephethethowo@gmail.com>
2025-01-17 21:37:12 +0100

    Initial commit

---
+++ generate-keypair.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var web3_js_1 = require("@solana/web3.js");
+var keypair = web3_js_1.Keypair.generate();
+console.log(keypair);
